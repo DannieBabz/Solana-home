98cef39b341d4a1b2c3290ec23606bb674ea402f
Babatunde Daniel <[similoluwababatunde@gmail.com]>
2023-06-15 14:30:44 +0100

    [Adding functional hamburger menu]

--- src/script.js
+++ src/script.js
@@ -1,2 +1,8 @@
-mynav = document.getElementById("myNav");
-console.log(mynav);
+const mynav = document.getElementById("myNav");
+const hamBtn = document.getElementById("hamBtn");
+const body = document.getElementById("body");
+
+hamBtn.addEventListener("click", function () {
+	mynav.classList.toggle("hidden");
+	body.classList.toggle("overflow-y-hidden");
+});
