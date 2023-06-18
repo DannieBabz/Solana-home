45f9199911d4902751079581a03bc8ecc9f0b534
Babatunde Daniel <[similoluwababatunde@gmail.com]>
2023-06-18 19:26:35 +0100

    [Added Preloader to page]

--- src/script.js
+++ src/script.js
@@ -1,8 +1,15 @@
 const mynav = document.getElementById("myNav");
 const hamBtn = document.getElementById("hamBtn");
 const body = document.getElementById("body");
+const preLoader = document.getElementById("preloader");
+
+
+window.addEventListener("load", function () {
+	preLoader.classList.add("hidden");
+});
 
 hamBtn.addEventListener("click", function () {
 	mynav.classList.toggle("hidden");
 	body.classList.toggle("overflow-y-hidden");
 });
+
