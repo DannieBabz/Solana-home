c2b91c98ceb753960befb7799711f5f52d06c6d3
Babatunde Daniel <[similoluwababatunde@gmail.com]>
2023-06-19 16:51:50 +0100

    [Adjusted some irregularities in mobile navigation]

--- src/script.js
+++ src/script.js
@@ -3,13 +3,14 @@ const hamBtn = document.getElementById("hamBtn");
 const body = document.getElementById("body");
 const preLoader = document.getElementById("preloader");
 
-
 window.addEventListener("load", function () {
 	preLoader.classList.add("hidden");
+	body.classList.add("overflow-y-scroll");
 });
 
 hamBtn.addEventListener("click", function () {
 	mynav.classList.toggle("hidden");
+	mynav.classList.toggle("flex");
+	body.classList.toggle("fixed");
 	body.classList.toggle("overflow-y-hidden");
 });
-
